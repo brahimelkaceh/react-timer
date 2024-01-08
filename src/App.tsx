e0915e3b20e765945cd428b29663c097e8ed53af
brahimelkaceh <brahimelkaceh@gmail.com>
2024-01-08 17:20:37 +0100

    start working on react timer application using React + typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,17 @@
-import Input from "./components/Input";
-import Form, { FormHandle } from "./components/Form";
-import Button from "./components/Button";
-import { useRef } from "react";
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
 
-const App = () => {
-  const customForm = useRef<FormHandle>(null);
-  const handleSave = (data: unknown) => {
-    const extractdData = data as { userName: string; userAge: number };
-    console.log(extractdData);
-    customForm.current?.clear();
-  };
+function App() {
   return (
     <>
+      <Header />
       <main>
-        <h1>Advanced Components Types</h1>
-        <Form onSave={handleSave} ref={customForm}>
-          <Input id="userName" label="enter user Name" type="text" />
-          <Input id="userAge" label="enter user Age" type="number" />
-          <p>
-            <Button type="submit">Save</Button>
-          </p>
-        </Form>
+        <AddTimer />
+        <Timers />
       </main>
     </>
   );
-};
+}
 
 export default App;
